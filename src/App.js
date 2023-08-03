c8a64a4ea60362504dfb54db5c11f139478084f4
sandrachima <sandraify28@gmail.com>
2023-08-04 00:32:19 +0100

    form
    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App () {
+
+
+ 
+const [FormData, setFormData] = React.useState({
+  email:'',
+  password:'',
+  confirmPassword:'',
+  joinedNewsletter:'false'
+})
+
+
+
+function handleChange(event) {
+  const {name, value, type, checked} = event.target
+  
+ //setFormData
+  setFormData(prevFormData => 
+    ({ ...prevFormData,
+  [name]:type === 'checked' ? checked : value
+}))
+
 }
+console.log(FormData)
+
+function handleSubmit(event){
+  event.preventDefault(event)
+  if (FormData.password === FormData.passwordConfirm){
+    console.log('Successfully signed up')
+  }
+    else {
+      console.log('password does not match')
+      return
+    }
+
+    if(FormData.joinedNewsletter){
+      console.log('thanks for signing up for our newsletter')
+    }
+  }
+
+
+
+  return (
+  
+  <div className="form-container">
+   <form className='form' onSubmit={handleSubmit}>
+    <input  type = 'email'
+    placeholder = 'Email address'
+    className = 'form-input'
+    name = 'email'
+    onChange = {handleChange}
+    value={FormData.email} />
+   
+  
+    
+    <input  type = 'password'
+    placeholder = 'password'
+    className = 'form-input'
+    name = 'password'
+    onChange = {handleChange}
+    value={FormData.password} />
+
+
+
+    <input  type = 'password'
+    placeholder = 'confirm password'
+    className = 'form-input'
+    name = 'passwordConfirm'
+    onChange = {handleChange}
+    value={FormData.passwordConfirm} />
+
+
+
+   <div className='form-marketing'>
+    <input 
+    id='okayToEmail'
+    type='checkbox'
+    name= 'joinNewsLetter'
+    onChange={handleChange}
+    value={FormData.password} 
+    checked ={FormData.joinedNewsletter}
+    />
+
+
+  <label htmlFor='okayToEmail' className='news'>i want to join the newsletter</label>
+  </div> 
+  <button className='submit'>Sign up</button>
+   
+
+      </form>
+     </div>
+     
+  )
+
+  }
 
 export default App;
